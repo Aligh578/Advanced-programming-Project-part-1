f70baf6a276fb9af141c853d16650a7c5747b505
Aligh677 <aligodratabadi677@gmail.com>
2026-06-03 14:33:58 +0330

    change the old graphic to new Graphic UI for board of chess

--- Main/Main.java
+++ Main/Main.java
@@ -1,14 +1,12 @@
 package Main;
 
+import javax.swing.SwingUtilities;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("[System] Initializing Chess Engine...");
-        
-        try {
-            Game game = new Game();
-            game.start();
-        } catch (Exception e) {
-            System.err.println("[Critical Error] Game terminated unexpectedly: " + e.getMessage());
-        }
+
+        SwingUtilities.invokeLater(() -> {
+            new ChessGUI();
+        });
     }
 }
