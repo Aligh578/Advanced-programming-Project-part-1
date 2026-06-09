751cd3d4833fc20734ca555106f422d89b58fce8
Aligh677 <aligodratabadi677@gmail.com>
2026-06-09 21:30:55 +0330

    feat: implement En Passant capture logic and board move memory

--- Pieces/Pawn.java
+++ Pieces/Pawn.java
@@ -10,18 +10,43 @@ public class Pawn extends Piece {
 
     @Override
     public boolean isValidMove(int startR, int startC, int endR, int endC, Board board) {
-        int direction = (this.color == Color.WHITE) ? -1 : 1;
-        int startingRow = (this.color == Color.WHITE) ? 6 : 1;
+        int direction = (this.getColor() == Utils.Color.WHITE) ? -1 : 1;
+        int startingRow = (this.getColor() == Utils.Color.WHITE) ? 6 : 1;
 
         if (startC == endC && endR == startR + direction) {
             return board.getPiece(endR, endC) == null;
         }
+        
         if (startC == endC && startR == startingRow && endR == startR + (2 * direction)) {
             return board.getPiece(startR + direction, startC) == null && board.getPiece(endR, endC) == null;
         }
+        
+
         if (Math.abs(startC - endC) == 1 && endR == startR + direction) {
-            return board.getPiece(endR, endC) != null;
+            
+
+            if (board.getPiece(endR, endC) != null) {
+                return true;
+            }
+            
+
+            if (board.getPiece(endR, endC) == null) {
+                Piece adjacentPiece = board.getPiece(startR, endC);
+                
+                if (adjacentPiece instanceof Pawn && adjacentPiece.getColor() != this.getColor()) {
+                    int expectedLastStartRow = startR + (direction * 2);
+                    
+
+                    if (board.getLastMoveStartRow() == expectedLastStartRow && 
+                        board.getLastMoveStartCol() == endC &&
+                        board.getLastMoveEndRow() == startR &&
+                        board.getLastMoveEndCol() == endC) {
+                        return true;
+                    }
+                }
+            }
         }
-        return false;
+    
+        return false; 
     }
 }
